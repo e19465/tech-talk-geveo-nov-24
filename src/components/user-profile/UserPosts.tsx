f58463c46773d3b886a15987b4d5f6474cbcccce
Sasindu Dilshan Rathnayaka <128506994+e19465@users.noreply.github.com>
2024-10-25 17:58:03 +0530

    UI finalized

--- src/components/user-profile/UserPosts.tsx
+++ src/components/user-profile/UserPosts.tsx
@@ -3,6 +3,7 @@ import SinglePost from "../post/SinglePost";
 const UserPosts = () => {
   return (
     <div className="px-8 py-20 flex flex-col items-center justify-center gap-8">
+      <h1 className="text-center text-2xl">My Posts</h1>
       <SinglePost />
       <SinglePost />
     </div>
